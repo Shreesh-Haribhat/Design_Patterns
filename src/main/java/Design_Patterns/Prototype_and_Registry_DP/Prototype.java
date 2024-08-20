a599648de941853ff2b33fb4038115cc658cc6de
Shreesh Haribhat <shreeshscaler@gmail.com>
2024-08-20 16:08:16 +0530

    Prototype and Registry

---
+++ src/main/java/Design_Patterns/Prototype_and_Registry_DP/Prototype.java
@@ -0,0 +1,6 @@
+package Design_Patterns.Prototype_and_Registry_DP;
+
+public interface Prototype<T> {
+
+    T clone();
+}
---
+++ src/main/java/Design_Patterns/Prototype_and_Registry_DP/Student.java
@@ -0,0 +1,87 @@
+package Design_Patterns.Prototype_and_Registry_DP;
+
+public class Student implements Prototype<Student>{
+
+    private int age;
+    private String name;
+    private double psp;
+    private String batch;
+    private double avgBatchPsp;
+
+    public Student(){}
+
+    public Student(Student student)
+    {
+        this.age = student.age;
+        this.name = student.name;
+        this.psp = student.psp;
+        this.batch = student.batch;
+        this.avgBatchPsp = student.avgBatchPsp;
+    }
+
+
+//    public int getAge() {
+//        return age;
+//    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+//    public String getName() {
+//        return name;
+//    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public double getPsp() {
+//        return psp;
+//    }
+
+    public void setPsp(double psp) {
+        this.psp = psp;
+    }
+
+//    public String getBatch() {
+//        return batch;
+//    }
+
+    public void setBatch(String batch) {
+        this.batch = batch;
+    }
+
+//    public double getAvgBatchPsp() {
+//        return avgBatchPsp;
+//    }
+
+    public void setAvgBatchPsp(double avgBatchPsp) {
+        this.avgBatchPsp = avgBatchPsp;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", psp=" + psp +
+                ", batch='" + batch + '\'' +
+                ", avgBatchPsp=" + avgBatchPsp +
+                '}';
+    }
+
+    @Override
+    public Student clone() {
+
+//        Student copy = new Student();
+//        copy.age = this.age;
+//        copy.name = this.name;
+//        copy.psp = this.psp;
+//        copy.batch = this.batch;
+//        copy.avgBatchPsp = this.avgBatchPsp;
+        //return copy;
+        return new Student(this);
+    }
+}
